d2d229faecfe23364de63dc26c6968df53e05fa5
Nagaraj <njhulmani@gmail.com>
2026-05-11 11:42:37 +0530

    updated production frontend UI

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -49,7 +49,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  // baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://securelearnlms-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
